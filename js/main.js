815d5af974390d55a99af8f64305e97d115f9c55
Maks_Hardy <maksyge@gmail.com>
2017-07-20 17:14:52 +0300

    main part done

---
+++ js/main.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    // slick slider TEAM
+    $('.slider').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+    // wow-animation
+    new WOW().init();
+
+    // TOP BUTTON
+    $(function() {
+
+        $(window).scroll(function() {
+
+            if ($(this).scrollTop() != 0) {
+
+                $('#toTop').fadeIn();
+
+            } else {
+
+                $('#toTop').fadeOut();
+
+            }
+
+        });
+
+        $('#toTop').click(function() {
+
+            $('body,html').animate({ scrollTop: 0 }, 800);
+
+        });
+    });
+
+    // slow motion
+    $('a[href*="#"]')
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) {
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1');
+                            $target.focus();
+                        };
+                    });
+                }
+            }
+        });
+});
